c8d994abda02bbbf7837fed611a6084a2d5b7375
Sujith Ranjitkumar <sujithranjitk@gmail.com>
2020-10-16 19:27:54 +1100

    QR component

---
+++ client/src/components/QRcode/QRcode.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import axios from "axios";
+import QRCode from "qrcode.react";
+
+class QR extends Component {
+        state = {
+            vendorDetails: []
+        };
+    
+    componentDidMount() {
+        this.getQrCode();
+    };
+
+    
+    getQrCode = () => {
+        axios
+        .get(`/api/businessVendor/${this.props.auth.user.id}`)
+        .then(response => {
+            const data = response.data;
+            this.setState({ vendorDetails: data });
+            console.log(this.state.vendorDetails.QR_link);
+        })
+        .catch(err => console.error(err));
+    };
+
+
+    render(){
+        const { user } = this.props.auth;
+
+        return (
+
+              <>
+              <QRCode value={`${this.state.vendorDetails.QR_link}`}/>
+              </>
+        );
+    }
+}
+
+QR.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+  });
+
+export default connect(mapStateToProps)(QR);
+
+
+
+
+
+
+
+
